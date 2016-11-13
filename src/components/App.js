3fd167a55121b0eb638da33eba1a50cc3410c2ad
Luis Boscan <luife642@gmail.com>
2016-11-13 12:26:33 -0400

    Video detail component completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from '.././components/SearchBar';
 import VideoList from '.././components/VideoList';
+import VideoDetail from '.././components/VideoDetail';
 
 const API_KEY = 'AIzaSyCm_53m3VzsEhZ5HJYFCyf0m-tzZ71B-70';
 
@@ -21,6 +22,7 @@ export default class App extends Component {
     return (
       <div>
        <SearchBar />
+       <VideoDetail video={this.state.videos[0]}/>
        <VideoList videos={this.state.videos} />
       </div>
     );
